0acb5f3da633bd8f876826e0570cacde71ff94a2
Arshal Abbas <arshalabbas123@gmail.com>
2020-11-20 04:59:32 -0800

    new resistor calculator added

---
+++ assets/script/script.js
@@ -0,0 +1,18 @@
+const value1 = document.getElementById("band1");
+const value2 = document.getElementById("band2");
+const valueMulti = document.getElementById("multi");
+const valueTol = document.getElementById("tol");
+const resultScreen = document.getElementById("result");
+
+function result() {
+    var band1 = value1.value;
+    var band2 = value2.value;
+    var multiplier = valueMulti.value;
+    var tolerance = valueTol.value;
+    var band12 = band1+band2;
+    var final =band12+multiplier;
+
+    resultScreen.innerHTML = `${final} ohm ±${tolerance}`;
+   // var multy = band12*multiplier;
+   // resultScreen.innerHTML = `${multy} ohm ±${tolerance}%`
+}
